eddf8b97333659126fc46ce92a921589963af2b3
SarahSibert <mail@sarahsibert.com>
2020-12-03 14:44:56 +0000

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 module.exports = {
   purge: ["src/**/*.html"],
   theme: {
-    extend: {},
+    // overwrite tailwind classes here
+    // colors:{
+    //   red: "#ff0000",
+    // },
+    opacity:{
+      '0':'0',
+      '20':'0.2',
+      '40':'0.4',
+      '60':'0.8',
+      '100':'1',
+    },
+    extend: {
+      // adds to the tailwind classes
+      spacing:{
+            sm:'8px',
+            md:'12px',
+            lg:'16px',
+          },
+      fontFamily:{
+        fun: ['Roboto', 'sans-serif'],
+      }
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 };
